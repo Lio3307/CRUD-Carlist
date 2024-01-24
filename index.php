15b5b4cac44cafca257c2301ad97409058a09684
Lio3307 <143271305+Lio3307@users.noreply.github.com>
2024-01-24 15:55:41 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_POST["submit"])) {
   $brg = $_POST['nmbrg'];
   $hrg = $_POST['hrga'];
   $date = $_POST['date'];
-  $com = $_POST['comp'];
+  $com = $_POST['company'];
 
   // Pengaturan untuk mengelola upload gambar
   $upload_dir = "pict/"; // Direktori tempat menyimpan gambar
@@ -25,12 +25,11 @@ if (isset($_POST["submit"])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="script.js"></script>r
+  <script src="script.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <title>coba</title>
 </head>
 <body>
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
